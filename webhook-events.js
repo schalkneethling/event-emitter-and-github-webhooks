c63365f0f89d265b1d899bcf40ac40b74c8c227a
Schalk Neethling <sneethling@mozilla.com>
2022-03-09 22:30:34 +0200

    feat:events new issue event handler
    Adds event emitter and handler for new issues

--- webhook-events.js
+++ webhook-events.js
@@ -1,6 +1,16 @@
+import EventEmitter from "events";
 export class WebhookEvents extends EventEmitter {
   constructor(opts = {}) {
     super(opts);
     this.name = opts.name;
   }
+}
+
+export function ouputNewIssueInfo(props) {
+  const message = `New issue opened: ${props.issueName}`;
+  const openedBy = `Issue opened by ${props.username}.`;
+  const newIssueCount = `Total open issues: ${props.openIssueCount}`;
+  console.info(message);
+  console.info(openedBy);
+  console.info(newIssueCount);
 }
